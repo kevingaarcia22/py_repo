de4125436d711c817ad30e4cd42febc867e81761
kevingaarcia22 <127509506+kevingaarcia22@users.noreply.github.com>
2023-03-18 15:52:14 +0300

    Add files via upload

---
+++ average.py
@@ -0,0 +1,11 @@
+# Define a list of numbers
+numbers = [4, 7, 2, 9, 5, 8]
+
+# Calculate the average of the numbers
+total = 0
+for number in numbers:
+    total += number
+average = total / len(numbers)
+
+# Print the average
+print("The average is:", average)
